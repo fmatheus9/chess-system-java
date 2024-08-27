ff3e0a665be61d2a13e9cf327cc2cd5a8525cb9a
fmatheus9 <matheus.fe.m@gmail.com>
2024-08-27 09:48:28 -0300

    starting to implement boardinerd card and piece

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
         Position position = new Position(3,5);
         System.out.println(position.toString());
+
+        Board board = new Board(8,8);
+        
     }
 }
